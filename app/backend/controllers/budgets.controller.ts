ad22f2ee4b126db09f8edc95f3ab5c115a046b57
Jason Rounds <jwrounds@usfca.edu>
2022-10-21 12:28:35 -0500

    updated controller handlers to utilize promises and express-async-handler to simplify error handling

--- app/backend/controllers/budgets.controller.ts
+++ app/backend/controllers/budgets.controller.ts
@@ -1,32 +1,48 @@
 import express from "express";
+import asyncHandler from "express-async-handler";
 
-const getBudgets = (_req: express.Request, res: express.Response): void => {
+// @desc    Get budgets
+// @route   GET api/v1/budgets
+// @access  Private
+const getBudgets = asyncHandler( async (_req: express.Request, res: express.Response) => {
   res.status(200).json({ message: "Get budgets!"});
-};
+});
 
-const postBudget = (req: express.Request, res: express.Response): void => {
+// @desc    Post budget
+// @route   POST api/v1/budgets
+// @access  Private
+const postBudget = asyncHandler( async (req: express.Request, res: express.Response) => {
   if (Object.keys(req.body).length === 0) {
     res.status(400);
     throw new Error("POST sent without valid request body!");
   }
   res.status(200).json({ message: "Post budget", json: req.body});
-};
+});
 
-const getBudgetById = (req: express.Request, res: express.Response): void => {
+// @desc    Get budget by its ID
+// @route   GET api/v1/budgets/:id
+// @access  Private
+const getBudgetById = asyncHandler( async (req: express.Request, res: express.Response) => {
   res.status(200).json({ message: `Get budget, id: ${req.params.id}`});
-};
+});
 
-const updateBudgetById = (req: express.Request, res: express.Response): void => {
+// @desc    Update budget by its ID
+// @route   PUT api/v1/budgets/:id
+// @access  Private
+const updateBudgetById = asyncHandler( async (req: express.Request, res: express.Response) => {
   if (Object.keys(req.body).length === 0) {
     res.status(400);
     throw new Error("PUT sent without valid request body!");
   }
   res.status(200).json({ message: `Update budget, id: ${req.params.id}`, json: req.body});
-};
+});
 
-const deleteBudgetById = (req: express.Request, res: express.Response): void => {
+// @desc    Delete budget by its ID
+// @route   DELETE api/v1/budgets/:id
+// @access  Private
+const deleteBudgetById = asyncHandler( async (req: express.Request, res: express.Response) => {
   res.status(200).json({ message: `Delete budget, id: ${req.params.id}`});
-};
+});
 
 export {
   getBudgets,
