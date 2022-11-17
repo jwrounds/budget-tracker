931d19a93dbf847727f89ed9ca3190f0074e2242
Jason Rounds <jwrounds@usfca.edu>
2022-11-17 15:37:17 -0800

    added basic ui

--- app/frontend/src/pages/Login.tsx
+++ app/frontend/src/pages/Login.tsx
@@ -1,6 +1,70 @@
+import React, { useState } from 'react';
+import { FaUser } from 'react-icons/fa';
+
+interface LoginState {
+    email: '',
+    password: '',
+}
+
 function Login() {
+  const [formData, setFormData] = useState<LoginState>({
+    email: '',
+    password: '',
+  });
+
+  const {email, password} = formData;
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }));
+  };
+
+  const onSubmit = (e: React.FormEvent) => {
+    console.log(e.target);
+    e.preventDefault();
+  };
+
   return (
-    <div>Login</div>
+    <>
+      <section className='heading'>
+        <h1>
+          <FaUser /> Login
+        </h1>
+        <p>Sign in to your account</p>
+      </section>
+
+      <section className='form'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <input 
+              type="text" 
+              className="form-control" 
+              id='email' 
+              name='email' 
+              value={email} 
+              placeholder='Enter your email'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input 
+              type="text" 
+              className="form-control" 
+              id='password' 
+              name='password' 
+              value={password} 
+              placeholder='Enter your password'
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
--- app/frontend/src/pages/Register.tsx
+++ app/frontend/src/pages/Register.tsx
@@ -1,6 +1,95 @@
+import React, { useState } from 'react';
+import { FaUser } from 'react-icons/fa';
+
+interface RegistrationState {
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+}
+
 function Register() {
+  const [formData, setFormData] = useState<RegistrationState>({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const {name, email, password, password2} = formData;
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }));
+  };
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>Register</div>
+    <>
+      <section className='heading'>
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+
+      <section className='form'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <input 
+              type="text" 
+              className="form-control" 
+              id='name' 
+              name='name' 
+              value={name} 
+              placeholder='Enter your name'
+              onChange={onChange}
+            />         
+          </div>
+          <div className='form-group'>
+            <input 
+              type="text" 
+              className="form-control" 
+              id='email' 
+              name='email' 
+              value={email} 
+              placeholder='Enter your email'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input 
+              type="text" 
+              className="form-control" 
+              id='password' 
+              name='password' 
+              value={password} 
+              placeholder='Enter your password'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input 
+              type="text" 
+              className="form-control" 
+              id='password2' 
+              name='password2' 
+              value={password2} 
+              placeholder='Re-enter your password'
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
